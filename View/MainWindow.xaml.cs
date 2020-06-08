4d674a5554f6d9c3793feb31c4d2a04973720fe7
lowkenge <low-kg@hotmail.com>
2020-06-08 15:38:11 +0800

    JSON Parsing

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -13,9 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using BoostOrderInterviewQuestion.Controller;
 using BoostOrderInterviewQuestion.Model;
 using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
 
 namespace BoostOrderInterviewQuestion
 {
@@ -28,9 +30,7 @@ namespace BoostOrderInterviewQuestion
         public MainWindow()
         {
             InitializeComponent();
-            CartController controller = new CartController();
-            controller.start_get();
-            
+            start_get();
         }
 
         private void deductQty_Click(object sender, RoutedEventArgs e)
@@ -63,16 +63,52 @@ namespace BoostOrderInterviewQuestion
 
         }
 
-        //protected void viewData()
-        //{
-        //    string jsonData = @"{  
-        //    'FirstName':'Jignesh',  
-        //    'LastName':'Trivedi'  
-        //    }";
-        //    dynamic data = JObject.Parse(jsonData);
+        public void start_get()
+        {
+            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format("https://mangomart-autocount.myboostorder.com/wp-json/wc/v1/products"));
 
-        //    Console.WriteLine(string.Concat("Hi ", data.FirstName, " " + data.LastName));
-        //    Console.ReadLine();
-        //}
+            WebReq.Method = "GET";
+            WebReq.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes("ck_2682b35c4d9a8b6b6effac126ac552e0bfb315a0:cs_cab8c9a729dfb49c50ce801a9ea41b577c00ad71"));
+            HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
+
+            string jsonString;
+            using (Stream stream = WebResp.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                jsonString = reader.ReadToEnd();
+            }
+
+            var js = new JavaScriptSerializer();
+            var json = js.Deserialize<List<CartData>>(jsonString);
+            foreach (var cartItem in jsonString)
+            {
+                Console.Write(cartItem.ToString());
+            }
+            //var newjsonString = jsonString.Replace("[", "");
+            //newjsonString = newjsonString.Replace("]", "");
+            ////var jsonData = JObject.Parse(newjsonString);
+            //Console.WriteLine(newjsonString);
+            dynamic data = JObject.Parse(jsonString);
+            data.name = this.qtyTxt.Text;
+            ////Console.WriteLine(json.Count);
+
+            //JavaScriptSerializer ser = new JavaScriptSerializer();
+            //var personlist = ser.Deserialize<List<Person>>(Json);
+            //return View(personlist);
+
+        }
+
+        protected void viewData()
+        {
+           
+            string jsonData = @"{  
+            'FirstName':'Jignesh',  
+            'LastName':'Trivedi'  
+            }";
+            dynamic data = JObject.Parse(jsonData);
+
+            Console.Write(string.Concat("Hi ", data.FirstName, " " + data.LastName));
+            Console.ReadLine();
+        }
     }
 }
